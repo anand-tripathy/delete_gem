271c748efe2d358242609d9b1d21a1713e8d7589
guptalakshya92 <guptalakshya92@gmail.com>
2020-04-10 08:05:29 +0530

    Update action.rb

--- action.rb
+++ action.rb
@@ -46,23 +46,23 @@ query {
 }
 GRAPHQL
 
-is_org = !"#{ENV['INPUT_ORGANISATION-NAME']}".nil? && !"#{ENV['INPUT_ORGANISATION-NAME']}".empty?
+is_org = (!"#{ENV['INPUT_ORGANISATION-NAME']}".nil? && !"#{ENV['INPUT_ORGANISATION-NAME']}".empty?)
 response = client.post '/graphql', {query: "#{(is_org ? org_query : repo_query)}"}.to_json
 ap response
 version_to_be_deleted = Kernel.const_get("#{ENV['INPUT_PACKAGE-NAME']}".capitalize)::VERSION
 puts "version to be deleted  #{version_to_be_deleted}"
-version_obj = response[:data][(is_org ? :organization : :repository)][:registryPackages][:nodes][0][:versions][:nodes].find {|x| x[:version] == version_to_be_deleted}
-
+version_obj = response[:data][(is_org ? :organization : :repository)][:registryPackages][:nodes][0][:versions][:nodes].find {|x| x[:version].to_s == version_to_be_deleted.to_s}
+puts "version object #{version_obj}"
 if !version_obj.nil?
-
+puts " inside if version present check with query #{mutation}"
   mutation = <<-GRAPHQL
-    mutation {
-      deletePackageVersion (input:{packageVersionId: #{version_obj[:id]}}){
-        success
-      }
-    }
-    GRAPHQL
-
+mutation {
+  deletePackageVersion (input:{packageVersionId: #{version_obj[:id]}}){
+     success
+   }
+ }
+GRAPHQL
+  puts " inside if version present check with query #{mutation}"
   mutation_response = client.post '/graphql', {query: mutation}.to_json
   
   mutation_response
